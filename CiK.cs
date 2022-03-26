a2de8e7eadf0560743ce2ef11bcb49539ebebbc9
Kazlex <57420344+Kazlex@users.noreply.github.com>
2022-03-26 15:10:04 +0100

    Add files via upload

---
+++ CiK.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekaZKLJ
+{
+    public static class Clanovi
+    {
+        public static string Ime { get; set; } = string.Empty;
+
+        public static string Prezime { get; set; } = string.Empty;
+
+        public static string Mail { get; set; } = string.Empty;
+    }
+
+    public static class Knige
+    {
+        public static string Naziv { get; set; } = string.Empty;
+
+        public static string Zanr { get; set; } = string.Empty;
+
+        public static DateTime Vreme { get; set; } = DateTime.Now;
+
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BibliotekaZKLJ
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Kupi(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Dodao si uspesno");
+        }
+
+        private void Vreme_TextInput(object sender, TextCompositionEventArgs e)
+        {
+            
+        }
+
+        private void Vreme_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
+        private void Vreme(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(DateTime.Now.ToString());
+        }
+
+        private void Registracija(object sender, RoutedEventArgs e)
+        {
+
+
+                Login log = new();
+                log.Owner = this;
+                log.ShowDialog();
+            
+        }
+    }
+}
